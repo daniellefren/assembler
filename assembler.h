f2ef5991be94419ca79d9229b7aa30d067838d55
daniellefrenklakh <danielle.frenklakh@gmail.com>
2024-06-15 13:06:29 +0300

    identify lines

---
+++ assembler.h
@@ -0,0 +1,8 @@
+//
+// Created by Danielle Frenklakh on 12/06/2024.
+//
+
+#ifndef ASSEMBLER_ASSEMBLER_H
+#define ASSEMBLER_ASSEMBLER_H
+
+#endif //ASSEMBLER_ASSEMBLER_H
---
+++ first_run.h
@@ -0,0 +1,16 @@
+//
+// Created by Danielle Frenklakh on 12/06/2024.
+//
+
+#ifndef ASSEMBLER_FIRST_RUN_H
+#define ASSEMBLER_FIRST_RUN_H
+
+
+
+class first_run {
+
+};
+
+
+
+#endif //ASSEMBLER_FIRST_RUN_H
