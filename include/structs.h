182875590a93c286b36705aaf8984a0bb805047f
daniellefrenklakh <danielle.frenklakh@gmail.com>
2024-08-02 09:27:00 +0300

    START fixing to new architecture

--- include/structs.h
+++ include/structs.h
@@ -9,7 +9,7 @@
 
 typedef struct {
     char label[MAX_LABEL_LENGTH];
-    int type; //.data or .string
+    enum directives type; //.data or .string
     char **value;
     size_t data_values_count; // count of data values
 } Directive
@@ -17,7 +17,7 @@ typedef struct {
 
 typedef struct {
     char name[MAX_LABEL_LENGTH];
-    int type; //command or data directive
+    enum instruction_types type; //command or data directive
     int address;
 } Label
 
@@ -38,8 +38,8 @@ typedef struct {
     char *command_name;
     enum opcode_command opcode_command_type; // enum containing enum values for opcode command types if it's an opcode
     unsigned int operand_number; //int containing the operand number
-    Operand first_operand; // TODO - change so it will work
-    Operand second_operand;
+    Operand src_operand; // TODO - change so it will work
+    Operand dst_operand;
 } Command;
 
 typedef struct {
@@ -47,17 +47,9 @@ typedef struct {
     size_t length;  // Length of the line (excluding null terminator)
     int instruction_type; //is it data directive or command
     int is_label; // is there a label with the command
-//    unsigned int operand_number; //int containing the operand number
     Command command;
     Directive directive;
     Label label;
-//    char *first_operand; // string containing the first operand r0-r7(can be null)
-//    int first_operand_type; // Label or register or number
-//    enum operand_classification_type first_operand_classification_type; // int containing enum values for first operand classification type
-//    char *second_operand; // string containing the second operand r0-r7(can be null)
-//    int second_operand_type; // Label or register or number
-//    enum operand_classification_type second_operand_classification_type; // int containing enum values for second_operand classification type
-//    enum opcode_command opcode_command_type; // enum containing enum values for opcode command types if it's an opcode
     char *binary_instruction; //the line in binary instruction
 } InstructionLine;
 
@@ -108,5 +100,5 @@ char *get_instruction_line_binary(LinesArray *linesArray, int number_of_line);
 void addInstructionLine(LinesArray *lines_array, InstructionLine *instruction_line);
 void addNewSymbol(SymbolTable *symbol_table, Symbol *symbol);
 void init_lines_array(LinesArray *lines_array, int initial_capacity);
-void init_symbol_table(SymbolTable *symbol_table, int initial_capacity);
+void init_label_table(SymbolTable *symbol_table, int initial_capacity);
 #endif
