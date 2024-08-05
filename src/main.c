02746cb094abb917437e544fdcea46acf6e5323f
daniellefrenklakh <danielle.frenklakh@gmail.com>
2024-08-05 20:01:31 +0300

    small fixes

--- src/main.c
+++ src/main.c
@@ -45,7 +45,7 @@ int main(int argc, char *argv[]) {
         printf("Line number %d: %s \n",i, assembly_lines_array->lines[i].line_content);
     }
 
-    start_second_run(assembly_lines_array);
+//    start_second_run(assembly_lines_array);
 
 
 
