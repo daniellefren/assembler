9d065117b863079f8c2c42d88cc83ebfb4a03eaa
royi66 <62953750+royi66@users.noreply.github.com>
2024-08-08 15:54:29 +0300

    Adress bug fix

--- src/main.c
+++ src/main.c
@@ -42,14 +42,12 @@ int main(int argc, char *argv[]) {
         fclose(file);
 
 
-
-
         for (int i = 0; i < assembly_lines_array->number_of_line; ++i) {
             printf("Line number %d: %s  ---- address=%d, binary line count= %d\n",i, assembly_lines_array->lines[i].line_content, assembly_lines_array->lines[i].starting_address, assembly_lines_array->lines[i].binary_line_count);
         }
 
-//        start_second_run(assembly_lines_array);
-//        printf("end %d file\n", file_number);
+        start_second_run(assembly_lines_array);
+        printf("end %d file\n", file_number);
     }
 
 
