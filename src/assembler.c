0bcfee058025121408a3031ab37cf1ebd0d9aa85
royi66 <62953750+royi66@users.noreply.github.com>
2024-08-20 20:12:13 +0300

    no free

--- src/assembler.c
+++ src/assembler.c
@@ -64,6 +64,6 @@ void run_assembler_on_file(char* filename, int file_number){
     /* Call the second_run function with the LinesArray table */
     start_second_run(assembly_lines_array, symbol_table, filename);
 
-    free_lines_array(assembly_lines_array);
-    free_symbol_table(symbol_table);
+    /*free_lines_array(assembly_lines_array); */
+    /*free_symbol_table(symbol_table);*/
 }
